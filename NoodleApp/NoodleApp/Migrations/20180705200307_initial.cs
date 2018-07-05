b0e73c81689e7e45debe6922e92607d67911752f
stephendeanharper <stephendeanharper@gmail.com>
2018-07-05 14:46:50 -0700

    deleted about us folder. adding in new migrations. updating home index

--- NoodleApp/NoodleApp/Migrations/20180705200307_initial.cs
+++ NoodleApp/NoodleApp/Migrations/20180705200307_initial.cs
@@ -14,8 +14,7 @@ namespace NoodleApp.Migrations
                     Id = table.Column<int>(nullable: false)
                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
                     Name = table.Column<string>(nullable: false),
-                    Country = table.Column<string>(nullable: true),
-                    Brand = table.Column<string>(nullable: true),
+                    BrandId = table.Column<int>(nullable: false),
                     Flavor = table.Column<string>(nullable: true),
                     ImgUrl = table.Column<string>(nullable: true),
                     Description = table.Column<string>(nullable: true)
@@ -37,27 +36,16 @@ namespace NoodleApp.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Reviews", x => x.ID);
-                    table.ForeignKey(
-                        name: "FK_Reviews_Noodles_NoodleId",
-                        column: x => x.NoodleId,
-                        principalTable: "Noodles",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
                 });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Reviews_NoodleId",
-                table: "Reviews",
-                column: "NoodleId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Reviews");
+                name: "Noodles");
 
             migrationBuilder.DropTable(
-                name: "Noodles");
+                name: "Reviews");
         }
     }
 }
